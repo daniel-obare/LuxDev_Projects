e939d89ec51a4a03dd5e0b77676e8b7d29f39b00
Daniel Obare <danielobare551@gmail.com>
2022-02-21 12:02:53 +0300

    leap year

---
+++ week_2_project/JS/index.js
@@ -0,0 +1,7 @@
+function isLeapYear() {
+    let year= document.getElementById("year").value;
+    
+    document.getElementById("GFG").innerHTML
+        = (year % 100 === 0) ? (year % 400 === 0)
+                            : (year % 4 === 0);
+}
